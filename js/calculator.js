a421ebe6d2b4545031dd754d2fb385344cd3f607
= <saleh.majeed8@gmail.com>
2020-10-21 21:19:37 +0530

    display function

--- js/calculator.js
+++ js/calculator.js
@@ -1,10 +1,11 @@
 const button = document.querySelector('.calc-buttons');
+const display_screen = document.querySelector('.screen')
 
 button.addEventListener('click', display);
 
 function display(event){
     button_value = event.target
     if(button_value.innerText >= 0 || button_value <= 9){
-        console.log(button_value.innerText)
+        display_screen.append(button_value.innerText)
     }
 }
