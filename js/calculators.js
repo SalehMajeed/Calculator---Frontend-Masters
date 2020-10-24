4f896a63793aa742746c7eff9f9409bbd3abe9b0
= <saleh.majeed8@gmail.com>
2020-10-24 18:58:31 +0530

    cases completed

--- js/calculators.js
+++ js/calculators.js
@@ -3,8 +3,29 @@ const calculator = document.getElementById('calculator');
 calculator.addEventListener('click', calculation);
 
 function calculation(event) {
-    
     const button_value = event.target.innerText;
-    console.log(button_value)
+    let operators;
+
+    if (button_value == '+') operators = '+';
+    else if (button_value == '÷') operators = '÷';
+    else if (button_value == '×') operators = '×';
+    else if (button_value == '-') operators = '-';
     
+    switch (button_value) {
+        case 'C':
+            console.log(button_value);
+            break;
+        case '←':
+            console.log(button_value);
+            break;
+        case '=':
+            console.log(button_value);
+            break;
+        case operators:
+            console.log(button_value);
+            break;
+        default:
+            console.log(button_value);
+            break;
+    }
 }
