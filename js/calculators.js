12a92522eb0ce762eac3fda44953c9a8910e93b0
= <saleh.majeed8@gmail.com>
2020-10-26 17:27:40 +0530

    calculator

--- js/calculators.js
+++ js/calculators.js
@@ -1,5 +1,6 @@
 const calculator = document.getElementById('calculator');
 const display = document.getElementById('display_screen');
+const operator_array = document.querySelectorAll('.operator_symbol');
 
 calculator.addEventListener('click', calculation);
 
@@ -12,8 +13,9 @@ function calculation(event) {
     button_value == '÷' ||
     button_value == '×' ||
     button_value == '-'
-  )
+  ) {
     operators = button_value;
+  }
 
   switch (button_value) {
     case 'C':
@@ -23,12 +25,14 @@ function calculation(event) {
       remove_value();
       break;
     case '=':
-      equal_button(operators);
+      equal_button();
       break;
     case operators:
+      loop_nodelist(operator_array, 'symbol');
       feed_value(button_value);
       break;
     default:
+      loop_nodelist(operator_array, 'number');
       feed_value(button_value);
       break;
   }
@@ -47,13 +51,28 @@ function remove_value() {
   display.innerText = display.innerText.slice(0, -1);
 }
 
-function equal_button(operators) {
+function equal_button() {
+  loop_nodelist(operator_array, 'number');
   let evaluate_result = display.innerText;
   let last_element = display.innerText.length - 1;
 
-  if (!Number(evaluate_result[last_element]))
-    display.innerText = evaluate_result.slice(0, -1);
+  if (!Number(evaluate_result[last_element])) {
+    evaluate_result = evaluate_result.replaceAll('×', '*').replaceAll('÷', '/');
+    display.innerText = eval(evaluate_result.slice(0, -1));
+    return;
+  }
 
   evaluate_result = evaluate_result.replaceAll('×', '*').replaceAll('÷', '/');
   display.innerText = eval(evaluate_result);
+}
+
+function loop_nodelist(list, type) {
+  if (type == 'symbol')
+    for (let i = 0; i < list.length; i++) {
+      list[i].setAttribute('disabled', 'disabled');
+    }
+  if (type == 'number')
+    for (let i = 0; i < list.length; i++) {
+      list[i].removeAttribute('disabled', 'disabled');
+    }
 }
