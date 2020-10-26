d0681e7565d89179744dcf98669ac7b6d941281d
= <saleh.majeed8@gmail.com>
2020-10-26 14:41:01 +0530

    operators solved

--- js/calculator.js
+++ js/calculator.js
@@ -8,84 +8,76 @@ let disabled_button = cases; // use cases instead
 button.addEventListener('click', display);
 
 function display(event) {
-    const button_value = event.target.innerText;
+  const button_value = event.target.innerText;
 
-    if (button_value == 'C') { // Use else if Or better use Switch
-        display_screen.innerText = '0';
+  if (button_value == 'C') {
+    // Use else if Or better use Switch
+    display_screen.innerText = '0';
+  }
+  if (button_value == '←') {
+    if (flag == true) {
+      // use map
+      disabled_button[0].removeAttribute('disabled');
+      disabled_button[1].removeAttribute('disabled');
+      disabled_button[2].removeAttribute('disabled');
+      disabled_button[3].removeAttribute('disabled');
     }
-    if (button_value == '←') {
-        if (flag == true) { // use map
-            disabled_button[0].removeAttribute('disabled');
-            disabled_button[1].removeAttribute('disabled');
-            disabled_button[2].removeAttribute('disabled');
-            disabled_button[3].removeAttribute('disabled');
-        }
-        if (display_screen.innerText.length == 1) {
-            display_screen.innerText = '0';
-        } else {
-            display_screen.innerText = display_screen.innerText.slice(0, -1);
-        }
+    if (display_screen.innerText.length == 1) {
+      display_screen.innerText = '0';
+    } else {
+      display_screen.innerText = display_screen.innerText.slice(0, -1);
     }
-    if (button_value == '=') {
-        if (
-            display_screen.innerText.length > 1 &&
-            (display_screen.innerText[display_screen.innerText.length - 1] ==
-                '×' ||
-                display_screen.innerText[display_screen.innerText.length - 1] ==
-                    '÷' ||
-                display_screen.innerText[display_screen.innerText.length - 1] ==
-                    '-' ||
-                display_screen.innerText[display_screen.innerText.length - 1] ==
-                    '+')
-        ) {
-            display_screen.innerText = display_screen.innerText.slice(0, -1);
-            if (flag == true) {
-                disabled_button[0].removeAttribute('disabled');
-                disabled_button[1].removeAttribute('disabled');
-                disabled_button[2].removeAttribute('disabled');
-                disabled_button[3].removeAttribute('disabled');
-            }
-        }
-        display_screen.innerText = display_screen.innerText.replaceAll(
-            '×',
-            '*'
-        );
-        display_screen.innerText = display_screen.innerText.replaceAll(
-            '÷',
-            '/'
-        );
-        let value_equal = eval(display_screen.innerText);
-        display_screen.innerText = value_equal;
-    }
-    if (button_value >= 0 || button_value <= 9) {
-        if (flag == true) {
-            disabled_button[0].removeAttribute('disabled');
-            disabled_button[1].removeAttribute('disabled');
-            disabled_button[2].removeAttribute('disabled');
-            disabled_button[3].removeAttribute('disabled');
-        }
-        if (display_screen.innerText[0] == '0') {
-            display_screen.innerText = '';
-            display_screen.append(button_value);
-        } else {
-            display_screen.append(button_value);
-        }
-    }
+  }
+  if (button_value == '=') {
     if (
-        button_value == '+' ||
-        button_value == '-' ||
-        button_value == '×' ||
-        button_value == '÷'
+      display_screen.innerText.length > 1 &&
+      (display_screen.innerText[display_screen.innerText.length - 1] == '×' ||
+        display_screen.innerText[display_screen.innerText.length - 1] == '÷' ||
+        display_screen.innerText[display_screen.innerText.length - 1] == '-' ||
+        display_screen.innerText[display_screen.innerText.length - 1] == '+')
     ) {
-        display_screen.append(button_value);
-        const symbols =
-            display_screen.innerText[display_screen.innerText.length - 1];
-        if (symbols) {
-            flag = true;
-            disabled_button[0].setAttribute('disabled', 'disabled');
-            disabled_button[1].setAttribute('disabled', 'disabled');
-            disabled_button[2].setAttribute('disabled', 'disabled');
-            disabled_button[3].setAttribute('disabled', 'disabled');
-        }
+      display_screen.innerText = display_screen.innerText.slice(0, -1);
+      if (flag == true) {
+        disabled_button[0].removeAttribute('disabled');
+        disabled_button[1].removeAttribute('disabled');
+        disabled_button[2].removeAttribute('disabled');
+        disabled_button[3].removeAttribute('disabled');
+      }
     }
+    display_screen.innerText = display_screen.innerText.replaceAll('×', '*');
+    display_screen.innerText = display_screen.innerText.replaceAll('÷', '/');
+    let value_equal = eval(display_screen.innerText);
+    display_screen.innerText = value_equal;
+  }
+  if (button_value >= 0 || button_value <= 9) {
+    if (flag == true) {
+      disabled_button[0].removeAttribute('disabled');
+      disabled_button[1].removeAttribute('disabled');
+      disabled_button[2].removeAttribute('disabled');
+      disabled_button[3].removeAttribute('disabled');
+    }
+    if (display_screen.innerText[0] == '0') {
+      display_screen.innerText = '';
+      display_screen.append(button_value);
+    } else {
+      display_screen.append(button_value);
+    }
+  }
+  if (
+    button_value == '+' ||
+    button_value == '-' ||
+    button_value == '×' ||
+    button_value == '÷'
+  ) {
+    display_screen.append(button_value);
+    const symbols =
+      display_screen.innerText[display_screen.innerText.length - 1];
+    if (symbols) {
+      flag = true;
+      disabled_button[0].setAttribute('disabled', 'disabled');
+      disabled_button[1].setAttribute('disabled', 'disabled');
+      disabled_button[2].setAttribute('disabled', 'disabled');
+      disabled_button[3].setAttribute('disabled', 'disabled');
+    }
+  }
 }
--- js/calculators.js
+++ js/calculators.js
@@ -4,49 +4,51 @@ const display = document.getElementById('display_screen');
 calculator.addEventListener('click', calculation);
 
 function calculation(event) {
-    const button_value = event.target.innerText;
-    let operators;
+  const button_value = event.target.innerText;
+  let operators;
 
-    if (button_value == '+') operators = '+';
-    else if (button_value == '÷') operators = '÷';
-    else if (button_value == '×') operators = '×';
-    else if (button_value == '-') operators = '-';
+  if (
+    button_value == '+' ||
+    button_value == '÷' ||
+    button_value == '×' ||
+    button_value == '-'
+  )
+    operators = button_value;
 
-    switch (button_value) {
-        case 'C':
-            display.innerText = '0';
-            break;
-        case '←':
-            remove_value();
-            break;
-        case '=':
-            equal_button(operators)
-            break;
-        case operators:
-            console.log(button_value);
-            break;
-        default:
-            feed_value(button_value);
-            break;
-    }
+  switch (button_value) {
+    case 'C':
+      display.innerText = '0';
+      break;
+    case '←':
+      remove_value();
+      break;
+    case '=':
+      equal_button(operators);
+      break;
+    case operators:
+      console.log(button_value);
+      break;
+    default:
+      feed_value(button_value);
+      break;
+  }
 }
 
 function feed_value(button_value) {
-    if (display.innerText == '0') display.innerText = '';
-    display.append(button_value);
+  if (display.innerText == '0') display.innerText = '';
+  display.append(button_value);
 }
 
 function remove_value() {
-    if (display.innerText.length <= 1) {
-        display.innerText = '0';
-        return;
-    }
-    display.innerText = display.innerText.slice(0, -1);
+  if (display.innerText.length <= 1) {
+    display.innerText = '0';
+    return;
+  }
+  display.innerText = display.innerText.slice(0, -1);
 }
 
-function equal_button(operators){
-    let evaluate_result = display.innerText
-    if (evaluate_result[evaluate_result.length - 1 == operators]){
-
-    }
+function equal_button(operators) {
+  let evaluate_result = display.innerText;
+  if (evaluate_result[evaluate_result.length - 1 == operators]) {
+  }
 }
